abeb47e977241b52d84050c856553be1db876924
abood.jarrar <abd.a.jarrar@gmail.com>
2025-07-21 01:08:10 +0300

    feat: finalyze implementing the mobile navigation functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// set current year
 const year = document.querySelector(".year");
 currentYear = new Date().getFullYear();
 year.textContent = currentYear;
+////////////////////////////////////////////////////////////////////////////////////
+// implementing the mobile navigation functionality
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
